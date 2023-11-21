d5b3380b60625afaf09e2ec965b6fc7b835eb325
Дмитрий <dimq1@ya.ru>
2023-11-22 00:12:29 +0300

    ml support

--- AudioNoteTranscription/Whisper/Inference.cs
+++ AudioNoteTranscription/Whisper/Inference.cs
@@ -1,22 +1,121 @@
-﻿using Microsoft.ML.OnnxRuntime.Tensors;
-using Microsoft.ML.OnnxRuntime;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using NReco.VideoConverter;
 using System.IO;
-using System.Security.RightsManagement;
-using System.Windows.Markup;
-using System.Diagnostics;
+using System.Linq;
 using System.Net;
+using Microsoft.ML.OnnxRuntime;
+using Microsoft.ML.OnnxRuntime.Tensors;
+using NReco.VideoConverter;
 
 namespace AudioNoteTranscription.Whisper
 {
     public static class Inference
     {
-
+        public static Dictionary<int, string> ALL_LANGUAGE_TOKENS => new Dictionary<int, string>
+        {
+            { 50259, "en" },
+            { 50260, "zh" },
+            { 50261, "de" },
+            { 50262, "es" },
+            { 50263, "ru" },
+            { 50264, "ko" },
+            { 50265, "fr" },
+            { 50266, "ja" },
+            { 50267, "pt" },
+            { 50268, "tr" },
+            { 50269, "pl" },
+            { 50270, "ca" },
+            { 50271, "nl" },
+            { 50272, "ar" },
+            { 50273, "sv" },
+            { 50274, "it" },
+            { 50275, "id" },
+            { 50276, "hi" },
+            { 50277, "fi" },
+            { 50278, "vi" },
+            { 50279, "he" },
+            { 50280, "uk" },
+            { 50281, "el" },
+            { 50282, "ms" },
+            { 50283, "cs" },
+            { 50284, "ro" },
+            { 50285, "da" },
+            { 50286, "hu" },
+            { 50287, "ta" },
+            { 50288, "no" },
+            { 50289, "th" },
+            { 50290, "ur" },
+            { 50291, "hr" },
+            { 50292, "bg" },
+            { 50293, "lt" },
+            { 50294, "la" },
+            { 50295, "mi" },
+            { 50296, "ml" },
+            { 50297, "cy" },
+            { 50298, "sk" },
+            { 50299, "te" },
+            { 50300, "fa" },
+            { 50301, "lv" },
+            { 50302, "bn" },
+            { 50303, "sr" },
+            { 50304, "az" },
+            { 50305, "sl" },
+            { 50306, "kn" },
+            { 50307, "et" },
+            { 50308, "mk" },
+            { 50309, "br" },
+            { 50310, "eu" },
+            { 50311, "is" },
+            { 50312, "hy" },
+            { 50313, "ne" },
+            { 50314, "mn" },
+            { 50315, "bs" },
+            { 50316, "kk" },
+            { 50317, "sq" },
+            { 50318, "sw" },
+            { 50319, "gl" },
+            { 50320, "mr" },
+            { 50321, "pa" },
+            { 50322, "si" },
+            { 50323, "km" },
+            { 50324, "sn" },
+            { 50325, "yo" },
+            { 50326, "so" },
+            { 50327, "af" },
+            { 50328, "oc" },
+            { 50329, "ka" },
+            { 50330, "be" },
+            { 50331, "tg" },
+            { 50332, "sd" },
+            { 50333, "gu" },
+            { 50334, "am" },
+            { 50335, "yi" },
+            { 50336, "lo" },
+            { 50337, "uz" },
+            { 50338, "fo" },
+            { 50339, "ht" },
+            { 50340, "ps" },
+            { 50341, "tk" },
+            { 50342, "nn" },
+            { 50343, "mt" },
+            { 50344, "sa" },
+            { 50345, "lb" },
+            { 50346, "my" },
+            { 50347, "bo" },
+            { 50348, "tl" },
+            { 50349, "mg" },
+            { 50350, "as" },
+            { 50351, "tt" },
+            { 50352, "haw" },
+            { 50353, "ln" },
+            { 50354, "ha" },
+            { 50355, "ba" },
+            { 50356, "jw" },
+            { 50357, "su" }
+        };
         public static List<NamedOnnxValue> CreateOnnxWhisperModelInput(WhisperConfig config)
         {
+            // Create a new DenseTensor with the desired shape
 
             var input = new List<NamedOnnxValue> {
                  NamedOnnxValue.CreateFromTensor("audio_stream", config.audio),
@@ -26,9 +125,8 @@ namespace AudioNoteTranscription.Whisper
                 NamedOnnxValue.CreateFromTensor("num_return_sequences", new DenseTensor<int>(new int[] {config.num_return_sequences}, new int[] { 1 })),
                 NamedOnnxValue.CreateFromTensor("length_penalty", new DenseTensor<float>(new float[] {config.length_penalty}, new int[] { 1 })),
                 NamedOnnxValue.CreateFromTensor("repetition_penalty", new DenseTensor<float>(new float[] {config.repetition_penalty}, new int[] { 1 })),
-                NamedOnnxValue.CreateFromTensor("attention_mask", config.attention_mask)
-
-            };
+                NamedOnnxValue.CreateFromTensor("decoder_input_ids", new DenseTensor<int>(new int[]{  50258 , 50263 /*ru*/, 50359, 50363 }, new int[] { 1, 4 } ))
+                };
 
             return input;
 
@@ -63,6 +161,7 @@ namespace AudioNoteTranscription.Whisper
             var sessionOptions = config.GetSessionOptionsForEp();
             sessionOptions.RegisterOrtExtensions();
 
+
             var session = new InferenceSession(config.WhisperOnnxPath, sessionOptions);
 
             List<string> outputs = new List<string>() { "str" };
@@ -89,7 +188,7 @@ namespace AudioNoteTranscription.Whisper
             var ffmpeg = new FFMpegConverter();
             var output = new MemoryStream();
 
-            var extension = Path.GetExtension(file).Substring(1);
+            var extension = System.IO.Path.GetExtension(file).Substring(1);
 
             // Convert to PCM
             ffmpeg.ConvertMedia(inputFile: file,
